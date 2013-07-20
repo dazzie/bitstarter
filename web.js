4372d0963923093ca522a88a19e7b3481d5bea51
Daragh Moran <moran.daragh@gmail.com>
2013-07-20 17:48:04 +0000

    udpated web.js

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+/**
+* Module dependencies.
+*/
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response){
+response.send(fs.readFileSync( './index.html').toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// all environments
+var port= process.env.PORT || 5000;
+
+app.listen(port, function(){
+    console.log('Listening on port ' + port);
 });
